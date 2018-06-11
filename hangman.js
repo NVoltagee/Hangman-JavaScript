8e69cd996b684de42103f3a2f4cb2547992a5578
Norman Trujillo <natrujillo9@gmail.com>
2018-06-11 11:59:04 -0400

    Formatted code.

--- hangman.js
+++ hangman.js
@@ -1,9 +1,9 @@
 prompt = require("prompt-sync")();
 
 console.log("Welcome to Hangman!");
-console.log("Guess the music instrument.")
+console.log("Guess the music instrument.");
 //Array que guarda las palabras para el juego.
-var words = ["guitar","piano","maracas"];
+var words = ["guitar", "piano", "maracas"];
 //Escoge una palabra al azar del Array.
 var word = words[Math.floor(Math.random() * words.length)];
 
@@ -11,26 +11,26 @@ var answerArray = [];
 
 //Crea el loop
 for (var i = 0; i < word.length; i++) {
-	answerArray[i] = "_";
+  answerArray[i] = "_";
 }
 
 var remainingLetters = word.length;
 
 while (remainingLetters > 0) {
-	console.log(answerArray.join(" "));
-	var guess = prompt("Guess a letter");
-	if (guess === null) {
-	break;
-} else if (guess.length !== 1) {
-	console.log("Please enter a single letter.");
-} else {
-	for (var j = 0; j < word.length; j++) {
-if (word[j] === guess) {
- 	answerArray[j] = guess;
- 	remainingLetters--;
-}
-}
-}
+  console.log(answerArray.join(" "));
+  var guess = prompt("Guess a letter");
+  if (guess === null) {
+    break;
+  } else if (guess.length !== 1) {
+    console.log("Please enter a single letter.");
+  } else {
+    for (var j = 0; j < word.length; j++) {
+      if (word[j] === guess) {
+        answerArray[j] = guess;
+        remainingLetters--;
+      }
+    }
+  }
 }
 
 console.log(answerArray.join(" "));
